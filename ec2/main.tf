ebc4353be1de8f402a027e3cd18ca35491f4e194
Venkat431 <venkat.aella@gmail.com>
2023-03-20 14:16:03 -0700

    roboshop-setup

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,7 +10,7 @@ resource "aws_spot_instance_request" "ec2" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags                   = {
-    name = var.component["name"]
+    name = var.component
   }
   provisioner "remote-exec" {
 
