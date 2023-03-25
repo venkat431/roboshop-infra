9cca84b33802bbbafed3a44933b618cf4bf50407
Venkat431 <venkat.aella@gmail.com>
2023-03-25 15:35:34 -0700

    roboshop-setup

--- ec2/main.tf
+++ ec2/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
-  wait_for_fulfillment   = true
+ # wait_for_fulfillment   = true
   tags                   = {
     Name = var.component
 
