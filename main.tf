ba1345dd94df13cc1975e4c1fadd653584013f95
Venkat431 <venkat.aella@gmail.com>
2023-03-25 13:51:17 -0700

    roboshop-setup

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2" {
   component     = each.value["name"]
   instance_type = each.value["type"]
   tags          = {
-    name = each.value[var.component]
+    name = var.component
   }
 }
 
